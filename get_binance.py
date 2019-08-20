83cb52c0121ac3eebfc13bca830b67d301a38ad3
Andrew Berg <berg@squizzer.io>
2019-08-20 16:39:09 +0300

    Implemented function to get data from binance

---
+++ get_binance.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from binance.client import Client
+
+
+api_key = 'L8wvQXOvuHXcl2xMjiQ3vP7OPMg3G9wOMSzX86OTug7X9GwXHS80wgfWkAQfZB54'
+api_secret = 'rgp5iv5I0TRc12uqxSQH0APK9ISSHdmhrajtjKlqrghDQVHEhyD5UmAjcsiLzR1V'
+client = Client(api_key, api_secret)
+
+
+def get_all_binance(symbol, kline_size):
+    klines = client.get_historical_klines(symbol, kline_size, '10 August, 2019')
+    data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time',
+                                         'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+    # from milliseconds to seconds
+    data['timestamp'] = data['timestamp'] / 1000
+
+    return data.astype(float)
+
+
