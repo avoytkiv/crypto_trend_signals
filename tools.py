d73a7fbb5385f9c91fbc2401040e53700763fd7b
Andriy Mykhaylyshyn <amykh2007@gmail.com>
2019-08-21 22:51:49 +0300

    Removed commented code.

--- tools.py
+++ tools.py
@@ -9,28 +9,6 @@ import pandas as pd
 import json
 
 
-# def corrdot(*args, **kwargs):
-#     corr_r = args[0].corr(args[1], 'pearson')
-#     corr_text = f"{corr_r:2.2f}".replace("0.", ".")
-#     ax = plt.gca()
-#     ax.set_axis_off()
-#     marker_size = abs(corr_r) * 10000
-#     ax.scatter([.5], [.5], marker_size, [corr_r], alpha=0.6, cmap="coolwarm",
-#                vmin=-1, vmax=1, transform=ax.transAxes)
-#     font_size = abs(corr_r) * 40 + 5
-#     ax.annotate(corr_text, [.5, .5,],  xycoords="axes fraction",
-#                 ha='center', va='center', fontsize=font_size)
-#
-# df = get_history_kraken('XXBTZUSD', period)
-# sns.set(style='white', font_scale=1.6)
-# iris = calc_strategy(df)
-# iris = iris.loc[:, ['ma_points', 'stochastic', 'vol_points', 'candle']]
-# g = sns.PairGrid(iris, aspect=1.4, diag_sharey=False)
-# g.map_lower(sns.regplot, lowess=True, ci=False, line_kws={'color': 'black'})
-# g.map_diag(sns.distplot, kde_kws={'color': 'black'})
-# g.map_upper(corrdot)
-
-
 def send_post_to_telegram(type, channel_id, message):
     """
     :param
