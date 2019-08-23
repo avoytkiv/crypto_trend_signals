ef5500d7a03fad845333456556ff0a9a65a4c544
Andrew Berg <berg@squizzer.io>
2019-08-23 13:25:50 +0300

    Removed last row for active/incomplete candle in binance data

--- get_binance.py
+++ get_binance.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from binance.client import Client
+from binance.exceptions import BinanceAPIException
 
 
 api_key = 'L8wvQXOvuHXcl2xMjiQ3vP7OPMg3G9wOMSzX86OTug7X9GwXHS80wgfWkAQfZB54'
@@ -8,11 +9,14 @@ client = Client(api_key, api_secret)
 
 
 def get_all_binance(symbol, kline_size):
-    klines = client.get_historical_klines(symbol, kline_size, '10 August, 2019')
+    klines = client.get_historical_klines(symbol, kline_size, '15 August, 2019')
     data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time',
                                          'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-    # from milliseconds to seconds
+    # Remove last row of active candle
+    data = data.iloc[:-1]
+    # From milliseconds to seconds
     data['timestamp'] = data['timestamp'] / 1000
+    data['close_time'] = data['close_time'] + 1
 
     return data.astype(float)
 
